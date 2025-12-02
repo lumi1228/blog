f1f63c9ad884d46b7e9931fdd323a8e720f496c8
lumi <lumiya1228@gmail.com>
2025-12-02 23:28:36 +0800

    docs: uadd new content for interview preparation and front-end topics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,27 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
+import { sidebar } from './sidebar.js'
 
 export default defineUserConfig({
   bundler: viteBundler(),
-  base: '/blog/',  
+  colorMode: 'light',//'auto' | 'light' | 'dark'
+  // home: '/blog/',
+  base: '/',  
   lang: 'zh-CN',
   title: 'Lumi\'s Blog',
   description: '这是我的第一个 VuePress 站点',
   theme: defaultTheme({
+    // repo: 'https://github.com/lumi1228/blog',
     navbar: [
       { text: 'Home', link: '/' },
       {
         text: '前端基础',
+        prefix: '/fontend/',
         children: [
-          { text: '概览', link: '/fontend/navigate.md' },
-          { text: 'Javascript基础', link: '/fontend/class1/01.javascript.md' },
-          { text: '数据结构与算法', link: '/fontend/class2/01.data-structure.md' },
+          { text: '概览', link: 'navigate.md' },
+          { text: 'Javascript基础', link: 'class1/01.javascript.md' },
+          { text: '数据结构与算法', link: 'class2/01.data-structure.md' },
         ]
       },
       {
@@ -78,8 +83,8 @@ export default defineUserConfig({
           //     // { text: '自动化流程', link: '/fullstack/' },
           //   ]
           // },
-
-
+ 
+ 
           // {
           //   text: '前端进阶全栈基础',
           //   children: [
@@ -114,19 +119,17 @@ export default defineUserConfig({
         text: '剑指Offer',
         children: [
           { text: '面试概览', link: '/offer/README.md' },
-          { text: '笔试部分', 
-            children: [
-              { text: '笔试分类概览', link: '/offer/class0/README.md' },
-            ]
-          },
+          { text: '笔试部分', link: '/offer/class0/README.md'},
           { text: '基础八股文', 
             children: [
               { text: '概览', link: '/fontend/README.md' },
+              { text: 'HTTP网络请求', link: '/offer/class1/05.http.md'},
             ]
           },
           { text: '框架与原理', 
             children: [
-              { text: '概览', link: '/fontend/README.md' },
+              { text: 'vue使用', link: '/offer/class2/01.vue-use.md' },
+              { text: 'vue原理', link: '/offer/class2/02.vue-theory.md' },
             ]
           },
           { text: '项目与技术深度', 
@@ -143,87 +146,6 @@ export default defineUserConfig({
       },
       { text: 'GitHub', link: 'https://github.com/lumi1228' },
     ],
-    sidebar: {
-      '/fontend/class1': [
-        {
-          text: 'Javascript基础',
-          children: [
-            { text: 'JavaScript 简介', link: '/fontend/class1/02.javascript-introduce.md' },
-            { text: '基本概念', link: '/fontend/class1/03.basic-concept.md' },
-            { text: '数据类型', link: '/fontend/class1/04.data-type.md' },
-            { text: '操作符', link: '/fontend/class1/05.operator.md' },
-            { text: '变量、作用域和内存', link: '/fontend/class1/06.variable-scope-memory.md' },
-            { text: '面向对象的程序', link: '/fontend/class1/07.object-oriented-program.md' },
-            { text: '面向对象的程序之创建对象', link: '/fontend/class1/08.create-object.md' },
-            { text: '面向对象的程序设计之继承', link: '/fontend/class1/09.inheritance.md' },
-            { text: '函数表达式', link: '/fontend/class1/10.function-expression.md' },
-
-          ]
-        }
-      ],
-      '/fontend/class2': [
-        {
-          text: '数据结构与算法',
-          children: [
-            { text: '数据结构与算法简介', link: '/fontend/class2/01.data-structure.md' },
-            { text: '时间与空间复杂度', link: '/fontend/class2/02.time-space-complexity.md' },
-            { text: '栈', link: '/fontend/class2/03.stack.md' },
-            { text: '队列', link: '/fontend/class2/04.queue.md' },
-            { text: '链表', link: '/fontend/class2/05.linked-list.md' },
-            { text: '集合', link: '/fontend/class2/06.set.md' },
-            { text: '字典', link: '/fontend/class2/07.dictionary.md' },
-            { text: '树', link: '/fontend/class2/08.tree.md' },
-            { text: '图', link: '/fontend/class2/09.graph.md' },
-            { text: '堆', link: '/fontend/class2/10.heap.md' },
-            { text: '排序搜索', link: '/fontend/class2/11.sort-search.md' },
-            { text: '分而治之', link: '/fontend/class2/12.divide-and-conquer.md' },
-            { text: '动态规划', link: '/fontend/class2/13.dynamic-programming.md' },
-            { text: '贪心算法', link: '/fontend/class2/14.greedy-algorithm.md' },
-            { text: '回溯算法', link: '/fontend/class2/15.backtracking.md' },
-          ]
-        }
-      ],
-      '/fullstack/class2/': [
-        {
-          text: '工程化工具',
-          children: [
-            { text: '打包工具之webpack', link: '/fullstack/class2/03.tool-webpack.md' },
-            { text: '自动化工具之gulp', link: '/fullstack/class2/04.tool-gulp.md' },
-            { text: '脚手架生成器之Yeoman「持续更新」', link: '/fullstack/class2/05.tool-yeoman.md' },
-            { text: 'web框架之koa', link: '/fullstack/class2/06.koa-framework.md' },
-          ]
-        }
-      ],
-      '/fullstack/class3/': [
-        {
-          text: '项目全流程DevOps',
-          children: [
-            { text: '项目需求分析与工具', link: '/fullstack/class3/02.project-require.md' },
-            // { text: '自动化工具之gulp', link: '/fullstack/L1/04.tool-gulp.md' },
-            // { text: '脚手架生成器之Yeoman「持续更新」', link: '/fullstack/L1/05.tool-yeoman.md' },
-            // { text: 'web框架之koa', link: '/fullstack/L1/06.koa-framework.md' },
-          ]
-        },
-      ],
-      '/fullstack/class/': [
-        {
-          text: '项目全流程DevOps',
-          children: [
-            { text: '打包工具之webpack', link: '/fullstack/class1/02.project-require.md' },
-            // { text: '自动化工具之gulp', link: '/fullstack/L1/04.tool-gulp.md' },
-            // { text: '脚手架生成器之Yeoman「持续更新」', link: '/fullstack/L1/05.tool-yeoman.md' },
-            // { text: 'web框架之koa', link: '/fullstack/L1/06.koa-framework.md' },
-          ]
-        },
-        {
-          text: '认识大前端，全栈开发初体验',
-          children: [
-            { text: '1.前端困境与全栈破局', link: '/fullstack/L1/01.meet-fullstack.md' },
-            { text: '7.登录模块前端通用方案-持续更新', link: '/fullstack/L1/07.login-module.md' },
-            { text: '前端工程化概览', link: '/fullstack/L1/overview-engineering.md' },
-          ]
-        },
-      ]
-    }
-  }),
+    sidebar
+  })
 })
---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,94 @@
+export const sidebar = {
+  '/fontend/class1': [
+    {
+      text: 'Javascript基础',
+      children: [
+        { text: 'JavaScript 简介', link: '/fontend/class1/02.javascript-introduce.md' },
+        { text: '基本概念', link: '/fontend/class1/03.basic-concept.md' },
+        { text: '数据类型', link: '/fontend/class1/04.data-type.md' },
+        { text: '操作符', link: '/fontend/class1/05.operator.md' },
+        { text: '变量、作用域和内存', link: '/fontend/class1/06.variable-scope-memory.md' },
+        { text: '面向对象的程序', link: '/fontend/class1/07.object-oriented-program.md' },
+        { text: '面向对象的程序之创建对象', link: '/fontend/class1/08.create-object.md' },
+        { text: '面向对象的程序设计之继承', link: '/fontend/class1/09.inheritance.md' },
+        { text: '函数表达式', link: '/fontend/class1/10.function-expression.md' },
+
+      ]
+    }
+  ],
+  '/fontend/class2': [
+    {
+      text: '数据结构与算法',
+      children: [
+        { text: '数据结构与算法简介', link: '/fontend/class2/01.data-structure.md' },
+        { text: '时间与空间复杂度', link: '/fontend/class2/02.time-space-complexity.md' },
+        { text: '栈', link: '/fontend/class2/03.stack.md' },
+        { text: '队列', link: '/fontend/class2/04.queue.md' },
+        { text: '链表', link: '/fontend/class2/05.linked-list.md' },
+        { text: '集合', link: '/fontend/class2/06.set.md' },
+        { text: '字典', link: '/fontend/class2/07.dictionary.md' },
+        { text: '树', link: '/fontend/class2/08.tree.md' },
+        { text: '图', link: '/fontend/class2/09.graph.md' },
+        { text: '堆', link: '/fontend/class2/10.heap.md' },
+        { text: '排序搜索', link: '/fontend/class2/11.sort-search.md' },
+        { text: '分而治之', link: '/fontend/class2/12.divide-and-conquer.md' },
+        { text: '动态规划', link: '/fontend/class2/13.dynamic-programming.md' },
+        { text: '贪心算法', link: '/fontend/class2/14.greedy-algorithm.md' },
+        { text: '回溯算法', link: '/fontend/class2/15.backtracking.md' },
+      ]
+    }
+  ],
+  '/fullstack/class2/': [
+    {
+      text: '工程化工具',
+      children: [
+        { text: '打包工具之webpack', link: '/fullstack/class2/03.tool-webpack.md' },
+        { text: '自动化工具之gulp', link: '/fullstack/class2/04.tool-gulp.md' },
+        { text: '脚手架生成器之Yeoman「持续更新」', link: '/fullstack/class2/05.tool-yeoman.md' },
+        { text: 'web框架之koa', link: '/fullstack/class2/06.koa-framework.md' },
+      ]
+    }
+  ],
+  '/fullstack/class3/': [
+    {
+      text: '项目全流程DevOps',
+      children: [
+        { text: '项目需求分析与工具', link: '/fullstack/class3/02.project-require.md' },
+        // { text: '自动化工具之gulp', link: '/fullstack/L1/04.tool-gulp.md' },
+        // { text: '脚手架生成器之Yeoman「持续更新」', link: '/fullstack/L1/05.tool-yeoman.md' },
+        // { text: 'web框架之koa', link: '/fullstack/L1/06.koa-framework.md' },
+      ]
+    },
+  ],
+  '/fullstack/class/': [
+    {
+      text: '项目全流程DevOps',
+      children: [
+        { text: '打包工具之webpack', link: '/fullstack/class1/02.project-require.md' },
+        // { text: '自动化工具之gulp', link: '/fullstack/L1/04.tool-gulp.md' },
+        // { text: '脚手架生成器之Yeoman「持续更新」', link: '/fullstack/L1/05.tool-yeoman.md' },
+        // { text: 'web框架之koa', link: '/fullstack/L1/06.koa-framework.md' },
+      ]
+    },
+    {
+      text: '认识大前端，全栈开发初体验',
+      children: [
+        { text: '1.前端困境与全栈破局', link: '/fullstack/L1/01.meet-fullstack.md' },
+        { text: '7.登录模块前端通用方案-持续更新', link: '/fullstack/L1/07.login-module.md' },
+        { text: '前端工程化概览', link: '/fullstack/L1/overview-engineering.md' },
+      ]
+    },
+  ],
+  '/offer/class0/': [
+    {
+      text: '笔试分类概览',
+      children: [
+        { text: 'JS-代码题', link: '/offer/class0/01.JS-code.md' },
+        { text: 'ES-代码题', link: '/offer/class0/02.ES-code.md' },
+        { text: '读代码题', link: '/offer/class0/03.readcode.md' },
+
+      ]
+    }
+  ],
+}
+
